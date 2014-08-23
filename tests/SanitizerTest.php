15d40f47bb1f2b1dd114da1ac7f159dae5940ada
Jota Furtado <jotacfurtado@gmail.com>
2014-08-23 01:22:21 -0300

    Some Unit Tests

--- tests/SanitizerTest.php
+++ tests/SanitizerTest.php
@@ -18,6 +18,15 @@ class SanitizerTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('elyaD', $d['name']);
     }
 
+    public function testThatSanitizerCanSanitizeWithClosureAndParameters()
+    {
+        $s = new Sanitizer;
+        $s->register('substring', function($string, $start, $length) { return substr($string, $start, $length); });
+        $d = ['name' => 'Dayle'];
+        $s->sanitize(['name' => 'substring:2,3'], $d);
+        $this->assertEquals('yle', $d['name']);
+    }
+
     public function testThatSanitizerCanSanitizeWithClass()
     {
         $s = new Sanitizer;
@@ -25,6 +34,15 @@ class SanitizerTest extends PHPUnit_Framework_TestCase
         $d = ['name' => 'Dayle'];
         $s->sanitize(['name' => 'reverse'], $d);
         $this->assertEquals('elyaD', $d['name']);
+    }
+
+    public function testThatSanitizerCanSanitizeWithClassAndParameters()
+    {
+        $s = new Sanitizer;
+        $s->register('suffix', 'Suffix@sanitize');
+        $d = ['name' => 'Dayle'];
+        $s->sanitize(['name' => 'suffix:Rees'], $d);
+        $this->assertEquals('Dayle Rees', $d['name']);
     }
 
     public function testThatSanitizerCanSanitizeWithACallback()
@@ -36,12 +54,29 @@ class SanitizerTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('elyaD', $d['name']);
     }
 
+    public function testThatSanitizerCanSanitizeWithACallbackAndParameters()
+    {
+        $s = new Sanitizer;
+        $s->register('suffix', [new Suffix, 'sanitize']);
+        $d = ['name' => 'Dayle'];
+        $s->sanitize(['name' => 'suffix:Rees'], $d);
+        $this->assertEquals('Dayle Rees', $d['name']);
+    }
+
     public function testThatACallableRuleCanBeUsed()
     {
         $s = new Sanitizer;
         $d = ['name' => 'Dayle'];
         $s->sanitize(['name' => 'strrev'], $d);
         $this->assertEquals('elyaD', $d['name']);
+    }
+
+    public function testThatACallableRuleCanBeUsedWithParameters()
+    {
+        $s = new Sanitizer;
+        $d = ['number' => '2435'];
+        $s->sanitize(['number' => 'str_pad:10,0,0'], $d);
+        $this->assertEquals('0000002435', $d['number']);
     }
 
     public function testThatSanitizerFunctionsWithMultipleRules()
@@ -53,6 +88,18 @@ class SanitizerTest extends PHPUnit_Framework_TestCase
         });
         $s->sanitize(['name' => 'strrev|alphabetize|trim'], $d);
         $this->assertEquals('elyaD', $d['name']);
+    }
+
+    public function testThatSanitizerFunctionsWithMultipleRulesWithParameters()
+    {
+        $s = new Sanitizer;
+        $d = ['name' => '  Dayle_ !'];
+        $s->register('suffix', [new Suffix, 'sanitize']);
+        $s->register('alphabetize', function($field) {
+            return preg_replace('/[^a-zA-Z]/', null, $field);
+        });
+        $s->sanitize(['name' => 'suffix: Rees |strrev|alphabetize|trim'], $d);
+        $this->assertEquals('seeRelyaD', $d['name']);
     }
 
     public function testThatGlobalRulesCanBeSet()
@@ -71,6 +118,24 @@ class SanitizerTest extends PHPUnit_Framework_TestCase
             'last_name' => 'seer',
         ], $d);
     }
+
+    public function testThatGlobalRulesCanBeSetWithParameters()
+    {
+        $s = new Sanitizer;
+        $d = [
+            'first_name' => ' Dayle',
+            'last_name' => 'Rees ',
+        ];
+        $s->sanitize([
+            '*' => 'trim|strtolower|substr:1',
+            'last_name' => 'strrev',
+        ], $d);
+        $this->assertEquals([
+            'first_name' => 'ayle',
+            'last_name' => 'see',
+        ], $d);
+    }
+
 }
 
 class TestSanitizer
@@ -78,5 +143,13 @@ class TestSanitizer
     public function foo($data)
     {
         return strrev($data);
+    }
+}
+
+class Suffix
+{
+    public static function sanitize($value, $suffix = '')
+    {
+        return $value . ' ' .  $suffix;
     }
 }
